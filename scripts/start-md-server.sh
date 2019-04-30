f7c65b557e8753ec4327c72266a8329e74f8fa7f
Tetricz <49681400+Tetricz@users.noreply.github.com>
2019-04-29 23:33:13 -0500

    Add files via upload

---
+++ scripts/start-md-server.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+if [ ! -f ${STEAMCMD}/steamcmd.sh ]; then
+    wget -q -O ${STEAMCMD}/steamcmd_linux.tar.gz http://media.steampowered.com/client/steamcmd_linux.tar.gz 
+    tar --directory ${STEAMCMD} -xvzf /opt/steamcmd/steamcmd_linux.tar.gz
+    rm ${STEAMCMD}/steamcmd_linux.tar.gz
+fi
+
+echo "update steamcmd"
+
+if [ ! -f "${STEAMCMD}/public/steambootstrapper_english.txt" ]; then
+    ${STEAMCMD}/steamcmd.sh \
+    +login anonymous \
+    +quit
+fi
+
+echo "download / update game"
+
+if [ "${VALIDATE}" == "" ]; then
+	${STEAMCMD}/steamcmd.sh \
+	+runscript updatemordhau \
+	+quit
+else
+	${STEAMCMD}/steamcmd.sh \
+	+runscript validatemordhau \
+	+quit
+fi
+
+echo "start-server"
+
+${GAME_DIR}/./MordhauServer.sh -log
