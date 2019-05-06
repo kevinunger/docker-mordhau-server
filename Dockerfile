546bc1ea32a99568dde4151fe2e2885e8d4e14d0
Tetricz <49681400+Tetricz@users.noreply.github.com>
2019-05-06 15:20:47 -0500

    Add files via upload

--- Dockerfile
+++ Dockerfile
@@ -5,32 +5,32 @@ MAINTAINER Tetricz
 RUN apt-get update
 RUN apt-get -y install curl lib32gcc1 libfontconfig1 gettext-base libpangocairo-1.0-0 libnss3 libgconf2-4 libxi6 libxcursor1 libxss1 libxcomposite1 libasound2 libxdamage1 libxtst6 libatk1.0-0 libxrandr2
 
-ENV DATA_DIR="/serverdata"
-ENV STEAMCMD="${DATA_DIR}/steamcmd"
-ENV GAME_DIR="${DATA_DIR}/mordhau"
-ENV QUERYPORT="27015"
-ENV GAME_PORT="7777"
-ENV VALIDATE="TRUE"
-ENV UID="99"
-ENV GID="100"
-ENV STEAMID64=""
-ENV PLAYER_COUNT="16"
-ENV SERVER_NAME="PLZ CHANGE"
-ENV SERVER_PASSWORD=""
-ENV ADMIN_PASSWORD=""
-ENV GAME_MODE=""
+ENV DATA_DIR="/serverdata" \
+	STEAMCMD="${DATA_DIR}/steamcmd" \
+	GAME_DIR="${DATA_DIR}/mordhau" \
+	QUERYPORT="27015" \
+	GAME_PORT="7777" \
+	VALIDATE="TRUE" \
+	UID="99" \
+	GID="100" \
+	STEAMID64="" \
+	PLAYER_COUNT="16" \
+	SERVER_NAME="PLZ CHANGE" \
+	SERVER_PASSWORD="" \
+	ADMIN_PASSWORD="" \
+	GAME_MODE="" \
+	SERVER=""
 
-RUN mkdir -p ${DATA_DIR}
-RUN mkdir -p ${STEAMCMD}
-RUN mkdir -p ${GAME_DIR}
-RUN useradd -d ${DATA_DIR} -s /bin/bash --uid ${UID} --gid ${GID} steam
-RUN chown -R ${UID}:${GID} ${DATA_DIR}
-
-RUN ulimit -n 2048
+RUN mkdir -p ${DATA_DIR} \
+	mkdir -p ${STEAMCMD} \
+	mkdir -p ${GAME_DIR} \
+	useradd -d ${DATA_DIR} -s /bin/bash --uid ${UID} --gid ${GID} steam \
+	chown -R ${UID}:${GID} ${DATA_DIR} \
+	ulimit -n 2048
 
 VOLUME /serverdata
 
-ADD /scripts/ /opt/scripts/
+COPY /scripts/ /opt/scripts/
 RUN chmod -R 770 /opt/scripts/
 RUN chown -R ${UID}:${GID} /opt/scripts
 
--- scripts/start-md-server.sh
+++ scripts/start-md-server.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+#---------------------
+#Install Steam Command
+#---------------------
 if [ ! -f ${STEAMCMD}/./steamcmd.sh ]; then
 	echo "steam not found -- downloading"
 	curl -o ${STEAMCMD}/steamcmd_linux.tar.gz "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz"
@@ -7,15 +11,12 @@ if [ ! -f ${STEAMCMD}/./steamcmd.sh ]; then
 else
 	echo "steam found"
 fi
+#---------------------
 
-echo "download / update game"
-
-if [ ! -f ${GAME_DIR}/Mordhau/Saved/Config/LinuxServer/Game.ini ]; then
-	export COPY_CONFIG="true"
-else
-	export COPY_CONFIG="false"
-fi
-
+#------------------------
+#Download and Update Game
+#------------------------
+echo "downloading / updating game"
 if [ "${VALIDATE}" == "" ]; then
 	${STEAMCMD}/./steamcmd.sh \
 	+login anonymous \
@@ -29,17 +30,119 @@ else
 	+app_update 629800 validate\
 	+quit 
 fi
+#------------------------
 
+#--------------------------------------------------------------------------------
+#Setting Gamemode -- If anyone knows a better way to do this. Please let me know.
+#--------------------------------------------------------------------------------
+if [ ! -f ${GAME_DIR}/Mordhau/Saved/Config/LinuxServer/Game${SERVER}.ini ]; then
+	if [ "${GAME_MODE}" == "FL" ]; then
+		export MAP1="MapRotation=FL_Taiga" \
+		MAP2="MapRotation=FL_MountainPeak" \
+		MAP3="MapRotation=FL_Camp" \
+		MAP4="MapRotation=FL_Grad"
+	fi
+fi
+
+if [ ! -f ${GAME_DIR}/Mordhau/Saved/Config/LinuxServer/Game${SERVER}.ini ]; then
+	if [ "${GAME_MODE}" == "SKM" ]; then
+		export MAP1="MapRotation=SKM_Contraband" \
+		MAP2="MapRotation=SKM_Camp" \
+		MAP3="MapRotation=SKM_Grad" \
+		MAP4="MapRotation=SKM_ThePit" \
+		MAP5="MapRotation=SKM_MountainPeak" \
+		MAP6="MapRotation=SKM_Tourney" \
+		MAP7="MapRotation=SKM_Tiaga"
+	fi
+fi
+
+if [ ! -f ${GAME_DIR}/Mordhau/Saved/Config/LinuxServer/Game${SERVER}.ini ]; then
+	if [ "${GAME_MODE}" == "SKM_64" ]; then
+		export MAP1="MapRotation=SKM_Taiga_64" \
+		MAP2="MapRotation=SKM_MountainPeak_64" \
+		MAP3="MapRotation=SKM_Grad_64" \
+		MAP4="MapRotation=SKM_Camp_64"
+	fi
+fi
+
+if [ ! -f ${GAME_DIR}/Mordhau/Saved/Config/LinuxServer/Game${SERVER}.ini ]; then
+	if [ "${GAME_MODE}" == "TDM" ]; then
+		export MAP1="MapRotation=TDM_Contraband" \
+		MAP2="MapRotation=TDM_Camp" \
+		MAP3="MapRotation=TDM_Grad" \
+		MAP4="MapRotation=TDM_ThePit" \
+		MAP5="MapRotation=TDM_MountainPeak" \
+		MAP6="MapRotation=TDM_Taiga" \
+		MAP7="MapRotation=TDM_Tourney"
+	fi
+fi
+
+if [ ! -f ${GAME_DIR}/Mordhau/Saved/Config/LinuxServer/Game${SERVER}.ini ]; then
+	if [ "${GAME_MODE}" == "TDM_64" ]; then
+		export MAP1="MapRotation=TDM_Contraband" \
+		MAP2="MapRotation=TDM_Camp" \
+		MAP3="MapRotation=TDM_Grad" \
+		MAP4="MapRotation=TDM_ThePit" \
+		MAP5="MapRotation=TDM_MountainPeak" \
+		MAP6="MapRotation=TDM_Taiga" \
+		MAP7="MapRotation=TDM_Tourney" 
+		MAP8="MapRotation=TDM_Taiga_64" \
+		MAP9="MapRotation=TDM_Camp_64"
+	fi
+fi
+
+if [ ! -f ${GAME_DIR}/Mordhau/Saved/Config/LinuxServer/Game${SERVER}.ini ]; then
+	if [ "${GAME_MODE}" == "FFA" ]; then
+		export MAP1="MapRotation=FFA_Contraband" \
+		MAP2="MapRotation=FFA_Camp" \
+		MAP3="MapRotation=FFA_Grad" \
+		MAP4="MapRotation=FFA_ThePit" \
+		MAP5="MapRotation=FFA_MountainPeak" \
+		MAP6="MapRotation=FFA_Taiga" \
+		MAP7="MapRotation=FFA_Tourney"
+	fi
+fi
+if [ ! -f ${GAME_DIR}/Mordhau/Saved/Config/LinuxServer/Game${SERVER}.ini ]; then
+	if [ "${GAME_MODE}" == "FFA" ]; then
+		export MAP1="MapRotation=HRD_Camp" \
+		MAP2="MapRotation=HRD_Grad" \
+		MAP3="MapRotation=HRD_Taiga" \
+		MAP4="MapRotation=HRD_MountainPeak"
+	fi
+fi
+
+if [ ! -f ${GAME_DIR}/Mordhau/Saved/Config/LinuxServer/Game${SERVER}.ini ]; then
+	if [ "${GAME_MODE}" == "FFA" ]; then
+		export MAP1="MapRotation=BR_Grad" \
+		MAP2="MapRotation=BR_Taiga"
+	fi
+fi
+#--------------------------------------------------------------------------------
+
+#-------------------------------------------
+#Copying default config and changes settings
+#-------------------------------------------
+echo "Checking Game.ini"
+
+if [ ! -f ${GAME_DIR}/Mordhau/Saved/Config/LinuxServer/Game${SERVER}.ini ]; then
+	export COPY_CONFIG="true"
+else
+	echo "Game.ini found"
+	export COPY_CONFIG="false"
+fi
 
 if [ "${COPY_CONFIG}" == "true" ]; then
 	mkdir -p ${GAME_DIR}/Mordhau/Saved/Config/LinuxServer
-	curl -o ${GAME_DIR}/temp.ini "https://raw.githubusercontent.com/Tetricz/docker-mordhau-server/master/scripts/Game.ini"
-	envsubst < "${GAME_DIR}/temp.ini" > "${GAME_DIR}/Mordhau/Saved/Config/LinuxServer/Game.ini"
-else
-	echo "Game.ini found"
-	echo "You will have to edit the file manually or delete it and restart the docker."
+	curl -o ${GAME_DIR}/temp.ini "https://raw.githubusercontent.com/Tetricz/docker-mordhau-server/experimental/scripts/Game.ini"
+	envsubst < "${GAME_DIR}/temp.ini" > "${GAME_DIR}/Mordhau/Saved/Config/LinuxServer/Game${SERVER}.ini"
+	rm -v "${GAME_DIR}/temp.ini"
+	curl -o ${GAME_DIR}/temp2.ini "https://raw.githubusercontent.com/Tetricz/docker-mordhau-server/experimental/scripts/Engine.ini"
+	cp -v "${GAME_DIR}/temp2.ini" "${GAME_DIR}/Mordhau/Saved/Config/LinuxServer/Engine${SERVER}.ini"
+	rm -v "${GAME_DIR}/temp2.ini"
 fi
+#--------------------------------------------
+
 
 echo "starting-server"
 
-${GAME_DIR}/./MordhauServer.sh -log -port=$GAME_PORT -queryport=$QUERYPORT
+${GAME_DIR}/./MordhauServer.sh -log -port=$GAME_PORT -queryport=$QUERYPORT -GAMEINI=${GAME_DIR}/Mordhau/Saved/Config/LinuxServer/Game${SERVER}.ini -ENGINEINI=${GAME_DIR}/Mordhau/Saved/Config/LinuxServer/Engine${SERVER}.ini
